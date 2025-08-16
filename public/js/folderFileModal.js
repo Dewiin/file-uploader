2a2aa67fc5665541e513d99982374e0a527bedc7
Devin Xie <devinxie418@gmail.com>
2025-08-16 01:31:34 -0400

    feat: designed upload file button; todo: implement backend

---
+++ public/js/folderFileModal.js
@@ -0,0 +1,15 @@
+// Add Folder
+const addFolderButton = document.querySelector(".sl-btn.new-folder");
+const addFolderModal = document.querySelector(".modal-container.new-folder");
+
+addFolderButton.addEventListener("click", () => {
+    addFolderModal.show();
+});
+
+// Add File
+const addFileButton = document.querySelector(".sl-btn.new-file");
+const addFileModal = document.querySelector(".modal-container.new-file");
+
+addFileButton.addEventListener("click", () => {
+    addFileModal.show();
+});
